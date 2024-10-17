0203334e2db3f4fd7bdd6823877e79431cee4cbe
Cassiyu <sakaicassio@gmail.com>
2024-10-17 10:40:59 -0300

    first commit

---
+++ src/main/java/br/com/fiap/javabet/game/GameService.java
@@ -0,0 +1,22 @@
+package br.com.fiap.javabet.game;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
+
+    public Game getGameById(Long id) {
+        return gameRepository.findById(id).orElseThrow(() -> new RuntimeException("Game not found"));
+    }
+}
