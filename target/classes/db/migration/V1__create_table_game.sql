b995cd8f07f0306257c646fdf1035f4a5ae1867c
Cassiyu <sakaicassio@gmail.com>
2024-10-17 10:46:53 -0300

    Projeto Corrigido 2

--- target/classes/db/migration/V1__create_table_game.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE games (
-       id SERIAL PRIMARY KEY,
-       home_team VARCHAR(255) NOT NULL,
-       away_team VARCHAR(255) NOT NULL,
-       home_team_score INT DEFAULT 0,
-       away_team_score INT DEFAULT 0
-);
--- target/classes/db/migration/V2__insert_games.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO games (home_team, away_team, home_team_score, away_team_score) VALUES
-('Brazil', 'Argentina', 0, 0),
-('Germany', 'France', 0, 0),
-('Spain', 'Portugal', 0, 0),
-('Italy', 'Netherlands', 0, 0),
-('England', 'Belgium', 0, 0);
